bfcbfd11be13d0db394712f758a3dfb5c7193af9
Tamrat demse <demsetamrat@gmail.com>
2025-06-11 04:54:19 +0300

    adding third VM for front end and implement cloneing IOT smart irrigation syatem front end

--- generate_hosts.py
+++ generate_hosts.py
@@ -2,10 +2,11 @@ import os
 from dotenv import load_dotenv
 from jinja2 import Template
 
-# Load .env file
+# Load .env file (ensure FRONTEND_IP and FRONTEND_USER are defined here)
 load_dotenv()
 
 # Read Jinja2 template
+# Ensure inventory/hosts.ini.j2 uses {{ frontend_ip }} and {{ frontend_user }}
 with open("inventory/hosts.ini.j2", "r") as f:
     template = Template(f.read())
 
@@ -15,6 +16,8 @@ rendered = template.render(
     webserver_user=os.getenv("WEBSERVER_USER"),
     dbserver_ip=os.getenv("DBSERVER_IP"),
     dbserver_user=os.getenv("DBSERVER_USER"),
+    frontend_ip=os.getenv("FRONTEND_IP"),   # Renamed from staticweb_ip
+    frontend_user=os.getenv("FRONTEND_USER"), # Renamed from staticweb_user
     python_path=os.getenv("PYTHON_PATH")
 )
 
